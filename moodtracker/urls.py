9ec66c92af9eba4bcd8bd4b15f15843e234c814a
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2022-07-30 22:18:54 +0300

    Add more views

--- moodtracker/urls.py
+++ moodtracker/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('rate/', views.rate, name='rate'),
+    path('results/', views.results, name='results'),
 ]
--- moodtracker/views.py
+++ moodtracker/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Mood
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Moodtracker index.")
+    return render(request, 'moodtracker/index.html')
+
+def rate(request):
+    return render(request, 'moodtracker/rate.html')
+
+def results(request):
+    latest_ratings = Mood.objects.order_by('date')
+    context = {
+        'latest_ratings': latest_ratings,
+    }
+    return render(request, 'moodtracker/results.html', context)
