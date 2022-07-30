d128ec3a10b91e1f08b4cbef1b86daec968ce2f9
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2022-07-30 23:26:30 +0300

    Add rating form

--- moodtracker/views.py
+++ moodtracker/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils import timezone
 
 from .models import Mood
 
@@ -8,10 +10,20 @@ def index(request):
     return render(request, 'moodtracker/index.html')
 
 def rate(request):
+    if request.method == 'POST':
+        try:
+            selected_rating = request.POST['rating']
+        except:
+            return render(request, 'moodtracker/rate.html', {
+                'error_message': "You didn't select a rating.",
+            })
+        new_rating = Mood.objects.create(rating=selected_rating, date=timezone.now())
+        new_rating.save()
+        return HttpResponseRedirect(reverse('results'))
     return render(request, 'moodtracker/rate.html')
 
 def results(request):
-    latest_ratings = Mood.objects.order_by('date')
+    latest_ratings = Mood.objects.all().order_by('date')
     context = {
         'latest_ratings': latest_ratings,
     }
